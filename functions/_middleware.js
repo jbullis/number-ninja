b9c8718f913522a81f14065f36cbea86f09273ea
jbullis <jacob.r.bullis@gmail.com>
2026-08-07 17:48:21 -0500

    Load K-5 compatibility guard after integration

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -5,7 +5,7 @@ export async function onRequest(context) {
   if (context.request.method !== 'GET' || !ct.includes('text/html') || !['/','/index.html'].includes(url.pathname)) return res;
   let html = await res.text();
   if (!html.includes('/js/curriculum.js')) html = html.replace('<script>', '<script src="/js/curriculum.js"></script>\n<script>');
-  if (!html.includes('/js/k5-integration.js')) html = html.replace('</body>', '<script src="/js/k5-integration.js"></script>\n</body>');
+  if (!html.includes('/js/k5-integration.js')) html = html.replace('</body>', '<script src="/js/k5-integration.js"></script>\n<script src="/js/k5-compat.js"></script>\n</body>');
   const headers = new Headers(res.headers); headers.delete('content-length');
   return new Response(html, { status: res.status, statusText: res.statusText, headers });
 }
